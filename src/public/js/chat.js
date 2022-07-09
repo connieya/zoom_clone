09bebf1932b2e18ebe9a6db2ed6abd6d538ac8fc
gunny6026 <gunny6026@naver.com>
2022-07-10 02:55:01 +0900

    feat : 닉네임과 채팅 메시지 구분하여 전송

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,7 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
@@ -9,6 +14,9 @@ socket.addEventListener("open", () => {
 
 socket.addEventListener("message", (m) => {
   console.log("Just got this : ", m.data, " from Server");
+  const li = document.createElement("li");
+  li.innerText = m.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -19,9 +27,24 @@ socket.addEventListener("close", () => {
 //   socket.send("hello from the browser !");
 // }, 3000);
 
+// makeMessage 함수를 이용해서 json 형식을 string 으로 변환해서
+// 서버로 보냄
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value); // 클라이언트에서 메시지를 보낸다.
+  socket.send(makeMessage("chat_message", input.value)); // 클라이언트에서 메시지를 보낸다.
   input.value = "";
+});
+
+// makeMessage 함수 사용하지 않고 json 을 바로 string 으로 변환해서
+// 클라이언트로 데이터 전송
+nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(
+    JSON.stringify({
+      type: "nickname",
+      payload: input.value,
+    })
+  );
 });
