785c49e22f5e98ff2de8ef16b2abafdaf0936e34
gunny6026 <gunny6026@naver.com>
2022-07-10 14:37:06 +0900

    feat : socket.io 사용 (back , front 연결 )

--- src/chatServer.js
+++ src/chatServer.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { json } from "express";
 import http from "http";
 import WebSocket from "ws";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -16,24 +17,49 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // http 서버 와 webSocket 서버 둘 다 사용하기 webSocket 서버만 사용해도 된다.
 // http 서버 위에 webSocket  서버를 만들 수 있도록 하기
 const server = http.createServer(app); // http 서버
-const wss = new WebSocket.Server({ server }); // webSocket 서버
+const io = SocketIO(server); // socket.io 서버 설정
 
-// fake database , 누가 연결되어 있는지 알기 위해서
+// web socket 코드  모두 주석 처리
+// const wss = new WebSocket.Server({ server }); // webSocket 서버
+
+/* // fake database , 누가 연결되어 있는지 알기 위해서
 // 서버에 연결된 브라우저를 담자
 const socket_info = [];
 
 wss.on("connection", (s) => {
+  console.log("connect ! ! ");
+  s["nickname"] = "Anon";
+  // 메시지를 보낼 때마다 추가 하는 것은 아니다.
+  // 그냥 새로운 클라이언트가 연결 될 때만 추가 된다
   socket_info.push(s); // firefox , chrome , edge 등 각 클라이언트를 담는다.
   s.on("close", () => {
     console.log("Disconnected from Client");
   });
   // s.send("hello !!");
+  // client 에서 string 으로 변환된 json object 를 보냄
   s.on("message", (m) => {
     // s.send(m); // 클라이언트에서 받은 채팅 메시지를 다시 클라이언트에게 보낸다
+    const message = JSON.parse(m);
 
-    // 연결된 모든 Socket에 접근하고 메시지를 보낸다.
-    socket_info.forEach((each_client) => each_client.send(m));
+    switch (message.type) {
+      case "chat_message":
+        // 연결된 모든 Socket에 접근하고 메시지를 보낸다.
+        socket_info.forEach((each_client) =>
+          each_client.send(`${s.nickname} : ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        // console.log("nickname = ", message.payload);
+        s["nickname"] = message.payload;
+        break;
+    }
   });
+}); */
+
+// 여기서 부터 socket.io 코드
+io.on("connection", (s) => {
+  // websocket 이 아니라 socketIO 의 socket 이다.
+  // console.log(s);
 });
 
 server.listen(3000, handleListen);
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,50 +1,63 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+// const socket = new WebSocket(`ws://${window.location.host}`);
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
-}
+// // 백엔드에서 어떤 프로그래밍 언어를 사용하는지 모르기 때문에
+// // object 를 string 으로 변환해줘야 한다.
+// function makeMessage(type, payload) {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
+// }
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
+// socket.addEventListener("open", () => {
+//   console.log("Connected from Server ✅");
+// });
 
-socket.addEventListener("message", (m) => {
-  console.log("Just got this : ", m.data, " from Server");
-  const li = document.createElement("li");
-  li.innerText = m.data;
-  messageList.append(li);
-});
+// socket.addEventListener("message", (m) => {
+//   console.log("Just got this : ", m.data, " from Server");
+//   const li = document.createElement("li");
+//   li.innerText = m.data;
+//   messageList.append(li);
+// });
 
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected from Server ❌");
+// });
 
-// setTimeout(() => {
-//   socket.send("hello from the browser !");
-// }, 3000);
+// // setTimeout(() => {
+// //   socket.send("hello from the browser !");
+// // }, 3000);
 
-// makeMessage 함수를 이용해서 json 형식을 string 으로 변환해서
-// 서버로 보냄
-messageForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("chat_message", input.value)); // 클라이언트에서 메시지를 보낸다.
-  input.value = "";
-});
+// // makeMessage 함수를 이용해서 json 형식을 string 으로 변환해서
+// // 서버로 보냄
+// messageForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   socket.send(makeMessage("chat_message", input.value)); // 클라이언트에서 메시지를 보낸다.
+//   const li = document.createElement("li");
+//   li.innerText = `나 : ${input.value}`;
+//   messageList.append(li);
+//   input.value = "";
+// });
 
-// makeMessage 함수 사용하지 않고 json 을 바로 string 으로 변환해서
-// 클라이언트로 데이터 전송
-nickForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(
-    JSON.stringify({
-      type: "nickname",
-      payload: input.value,
-    })
-  );
-});
+// // makeMessage 함수 사용하지 않고 json 을 바로 string 으로 변환해서
+// // 클라이언트로 데이터 전송
+// nickForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   socket.send(
+//     JSON.stringify({
+//       type: "nickname",
+//       payload: input.value,
+//     })
+//   );
+//   input.value = "";
+// });
+
+// 위에 코드 들이 webSocket 코드
+// 아래 부터 socket io 코드
+
+const socket = io(); // io 는 자동적으로 back-end socket.io 와 연결 해주는 function
+// 크롬 브라우저 콘솔 에 입력하면 io 라는 function 을 볼 수 있다.
+// io function이 알아서 socket.io 를 실행하고 있는 서버를 찾는다.
